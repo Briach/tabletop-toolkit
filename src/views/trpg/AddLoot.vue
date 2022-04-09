34948c28a2f77fa17161d947f8df41a8210f51c1
Fabian Brecht <fabian.brecht@web.de>
2022-04-09 16:58:21 +0200

    Removed inactive Systems

--- src/views/trpg/AddLoot.vue
+++ src/views/trpg/AddLoot.vue
@@ -120,7 +120,14 @@ export default class AddLoot extends Vue {
     }
 
     async getSystems(): Promise<void> {
+        var activeSystems: any[] = [];
         this.systemsResponse = await Fetcher.getSystems(this.$store.getters.apiUrl, true);
+        for (let i = 0; i < this.systemsResponse.body.length; i++) {
+            if (this.systemsResponse.body[i].active == true) {
+                activeSystems.push(this.systemsResponse.body[i]);
+            } 
+        } 
+        this.systemsResponse.body = activeSystems;
         this.currentSystem = this.systemsResponse.body[0];
     }
 
